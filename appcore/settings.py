57703b027c6009d6b6d5794db552d04e7079f874
Raju <rajuhosseng@gmail.com>
2024-09-21 00:34:52 +0600

    removed personal credential

--- appcore/settings.py
+++ appcore/settings.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_PORT = 25
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rajuhosseng@gmail.com'
-EMAIL_HOST_PASSWORD = 'scdq oico mkch ntpo'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
